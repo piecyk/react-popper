102e28a2c89c4c2ef5bf02ef69b1d4a9fb2153f3
Damian Pieczynski <piecyk@gmail.com>
2021-01-22 21:01:23 +0100

    Remove useIsMounted

--- src/usePopper.js
+++ src/usePopper.js
@@ -6,7 +6,7 @@ import {
   type VirtualElement,
 } from '@popperjs/core';
 import isEqual from 'react-fast-compare';
-import { fromEntries, useIsomorphicLayoutEffect, useIsMounted } from './utils';
+import { fromEntries, useIsomorphicLayoutEffect } from './utils';
 
 type Options = $Shape<{
   ...PopperOptions,
@@ -38,8 +38,6 @@ export const usePopper = (
     modifiers: options.modifiers || EMPTY_MODIFIERS,
   };
 
-  const checkIsMounted = useIsMounted();
-
   const [state, setState] = React.useState<State>({
     styles: {
       popper: {
@@ -60,8 +58,6 @@ export const usePopper = (
       enabled: true,
       phase: 'write',
       fn: ({ state }) => {
-        if (!checkIsMounted()) return;
-
         const elements = Object.keys(state.elements);
 
         setState({
@@ -75,7 +71,7 @@ export const usePopper = (
       },
       requires: ['computeStyles'],
     }),
-    [checkIsMounted]
+    []
   );
 
   const popperOptions = React.useMemo(() => {
